2d70ebd070fffdf2ad6a924585bdf5c8f6023d19
Nicholas Jitkoff <nicholas@jitkoff.com>
2020-04-17 07:13:10 -0700

    Stub out storage and browser action

--- src/background.js
+++ src/background.js
@@ -1,27 +1,39 @@
-// var whitelist = [
-//   /https:\/\/.*zoom\.us\/j\/.+?status=success/
-// ]
+var whitelist = [
+  // /https:\/\/.*zoom\.us\/j\/.+?status=success/
+]
 
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
-//   var url = tab.url;
-//   for (var pattern of whitelist) {
-//     if (pattern.test(url)) {
-//       console.log(pattern, url)
-//       // closeTab(tabId, 10000);
-//       break;
-//     }
-//   }
+var enabled;
+chrome.storage.sync.get({ enabled: true }, function(items) {
+  enabled = items.enabled
+});
+
+// chrome.browserAction.onClicked.addListener(function () {
+//   enabled = !enabled;
+//   chrome.storage.sync.set({ enabled: enabled }, function() {})
 // });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
+  var url = changeInfo.url;
+  if (!url) return;
+  for (var pattern of whitelist) {
+    if (pattern.test(url)) {
+      console.log(pattern, url)
+      closeTab(tabId, 10000);
+      break;
+    }
+  }
+});
 
 function closeTab(tabId, delay) {
   setTimeout(function() {
     chrome.tabs.remove(tabId)
-    }, delay || 3000)
+    }, delay || 5000)
 }
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action == "close") {
-      closeTab(sender.tab.id, 3000);
+      closeTab(sender.tab.id, 5000);
 
     }
   });
