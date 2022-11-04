12766465f1c70767e7923e11aae8f03ebb668e2f
Lawrence Luk <lluk@figma.com>
2022-11-04 16:26:53 -0600

    bugfixes

--- src/detect_default.js
+++ src/detect_default.js
@@ -1,1 +1,2 @@
+console.info("[Tab Recycler] Automatically closing this tab in 5 seconds");
 chrome.runtime.sendMessage({action: "close"}, function(response) {});
--- src/detect_figma.js
+++ src/detect_figma.js
@@ -1,8 +1,11 @@
 
-var xpath = "//div[text()='Open here instead']";
-var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-if (matchingElement) {
-  chrome.runtime.sendMessage({action: "close"}, function(response) {
-    console.log(response);
-  });
-}
+var xpath = "//div[contains(text(), 'in Figma app')]";
+
+setTimeout(() => {
+  var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+  if (matchingElement) {
+    console.info("[Tab Recycler] Automatically closing this tab in 5 seconds");
+    chrome.runtime.sendMessage({action: "close"}, function(response) {});
+  }
+}, 5000);
